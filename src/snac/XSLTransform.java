f098799f99c6e0753adb58bce8e6234400a7e01b
kgal <kjgalli@radium.ncsc.mil>
2019-01-04 15:33:40 +0000

    Seems to be mostly working except for post-process

--- src/snac/XSLTransform.java
+++ src/snac/XSLTransform.java
@@ -9,17 +9,17 @@ import java.net.URISyntaxException;
 
 public class XSLTransform {
     public static void main(String[] args) throws IOException, URISyntaxException, TransformerException {
-        if (args.length != 3){
-            System.out.println("Usage: <xsl-file> <xml-file> <out-file>");
+        if (args.length != 4){
+            System.out.println("Usage: <bases-dir> <xsl-file> <xml-file> <out-file>");
             return;
         }
         TransformerFactory factory = TransformerFactory.newInstance();
-        Source xslt = new StreamSource(new File(args[0]));
-        xslt.setParameter("basesdir", "/projects/bases");
-        xslt.setParameter("appendicize", "on");
+        Source xslt = new StreamSource(new File(args[1]));
         Transformer transformer = factory.newTransformer(xslt);
+        transformer.setParameter("basesdir", args[0]);
+        transformer.setParameter("appendicize", "on");
 
-        Source text = new StreamSource(new File(args[1]));
-        transformer.transform(text, new StreamResult(new File(args[2])));
+        Source text = new StreamSource(new File(args[2]));
+        transformer.transform(text, new StreamResult(new File(args[3])));
     }
 }
