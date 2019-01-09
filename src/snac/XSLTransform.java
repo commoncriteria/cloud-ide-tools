05e1a0028816523ee4a91d1b1db051cb59e445ee
kgal <kjgalli@radium.ncsc.mil>
2019-01-09 12:52:46 +0000

    Changed XSLTranform to allow for specification of parameters>

--- src/snac/XSLTransform.java
+++ src/snac/XSLTransform.java
@@ -7,19 +7,22 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+
 public class XSLTransform {
     public static void main(String[] args) throws IOException, URISyntaxException, TransformerException {
-        if (args.length != 4){
-            System.out.println("Usage: <bases-dir> <xsl-file> <xml-file> <out-file>");
+        if (args.length < 3){
+            System.out.println("Usage: <xsl-file> <xml-file> <out-file> [<param1-name>=<param1-value> [<param2-name>=<param2-value>[...]]]");
             return;
         }
         TransformerFactory factory = TransformerFactory.newInstance();
-        Source xslt = new StreamSource(new File(args[1]));
+        Source xslt = new StreamSource(new File(args[0]));
         Transformer transformer = factory.newTransformer(xslt);
-        transformer.setParameter("basesdir", args[0]);
-        transformer.setParameter("appendicize", "on");
+        for(int aa=3; aa<args.length; aa++){
+            String []nameAndVal=args[aa].split("=", 2);
+            transformer.setParameter(nameAndVal[0], nameAndVal[1]);
+        }
 
-        Source text = new StreamSource(new File(args[2]));
-        transformer.transform(text, new StreamResult(new File(args[3])));
+        Source text = new StreamSource(new File(args[1]));
+        transformer.transform(text, new StreamResult(new File(args[2])));
     }
 }
