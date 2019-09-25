0756ff9cc0886c970086653d25bc723ded782b67
Kevin G <kjgalli@radium.ncsc.mil>
2019-09-25 15:11:05 +0000

    Fixed setup.sh so that you can source it from anywhere and not have to be in the directory for it to work.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
-chmod u+x bin/*
+if [ "$BASH_SOURCE" = "" ] ; then
+    echo "This file should be sourced (or sourced with '. $1' shorthand)"
+    echo "and not executed or invoked with as an argument to a script."
+    exit 0
+fi
 
+if [ "${BASH_SOURCE}" = "${BASH_SOURCE%%/*}" ]; then
+    pushd .
+else
+    cd ${BASH_SOURCE%%/*}
+fi
+
+chmod u+x bin/*
 PATH+=:$(pwd)/bin
 
 [ -d bld ] || mkdir bld
 javac -d bld $(find src -name '*.java')
+
+popd
